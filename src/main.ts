426b0982e242195c9f63249dd1a350bc8791eabd
webcrawls <github.com@kaden.sh>
2024-10-19 14:40:52 -0400

    ✨ add a halfway decent demo page

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,45 @@ export const DATA_MARQUEE_Y_SPEED = "data-marquee-y"
 export const DATA_MARQUEE_EXTRA = "data-marquee-extra"
 export const DATA_MARQUEE_TILE = "data-marquee-tile"
 
-let marquees = []
+let marquees = {}
 
 const findElements = (parent: HTMLElement) => {
+
     const elements = parent.querySelectorAll(`[${DATA_MARQUEE_X_SPEED}], [${DATA_MARQUEE_Y_SPEED}]`)
     elements.forEach(el => {
+        if (el in marquees) return 
         console.log("making marquee", el)
         const marquee = createMarquee(el)
         marquee.start()
-        marquees.push(marquee)
-        console.log({marquee})
+        marquees[el] = marquee
     })
 }
 
-export const markii = () => setTimeout(() => findElements(document.body), 100)
+export interface Markii {
+    watch(): void
+    stop(): void
+}
+
+export const createMarkii = (root?: HTMLElement): Markii => {
+    if (!root) root = document.body
+
+    const handleUpdate = (mutations) => {
+        findElements(root)
+    }
+    
+    let observer: MutationObserver = new MutationObserver(handleUpdate)
+
+    const watch = () => {
+        observer.observe(root, {
+            subtree: true,
+            attributeFilter: [DATA_MARQUEE_X_SPEED, DATA_MARQUEE_Y_SPEED]
+        })
+    }
+
+    const stop = () => {
+        observer.disconnect()
+        // clear all Marquees?
+    }
+
+    return { watch, stop }
+}
